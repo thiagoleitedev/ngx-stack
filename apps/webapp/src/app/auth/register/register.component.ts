caf71eed4155d1798056ef86dbfe948c8c262235
Brannon N. Darby II <bdarby86@gmail.com>
2017-08-26 18:32:45 -0400

    fix(auth): fix register component

--- apps/webapp/src/app/auth/register/register.component.ts
+++ apps/webapp/src/app/auth/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { Subscription } from 'rxjs/Subscription'
+import { Store } from '@ngrx/store'
+import { AuthActions } from '../../state'
 import { NgxUiService } from '../../ui'
 import { Account, AccountApi } from '@ngx-plus/ngx-sdk'
 
@@ -9,54 +11,89 @@ import { Account, AccountApi } from '@ngx-plus/ngx-sdk'
   template: `
     <ngx-form [config]="formConfig"
               [item]="registration"
-              (action)="submit($event)">
+              (action)="handleAction($event)">
     </ngx-form>
   `,
-  styles: []
 })
 export class RegisterComponent implements OnInit {
-
   private subscriptions: Subscription[] = new Array<Subscription>()
   public registration: Account
   public formConfig: {}
 
   constructor(
     private ui: NgxUiService,
-    private userApi: AccountApi,
-    private router: Router,
-  ) { }
+    private store: Store<any>,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.formConfig = {
       fields: {
-        email: 'email',
-        password: 'password',
+        firstName: {
+          className: 'w-50',
+          type: 'input',
+          label: 'First Name',
+        },
+        middleName: {
+          className: 'w-50',
+          type: 'input',
+          label: 'Middle Name',
+        },
+        lastName: {
+          className: 'w-50',
+          type: 'input',
+          label: 'Last Name',
+        },
+        suffix: {
+          className: 'w-50',
+          type: 'select',
+          label: 'Suffix',
+          options: [
+            { label: 'Jr.', value: 'Jr.' },
+            { label: 'Sr.', value: 'Sr.' },
+            { label: 'II', value: 'II' },
+            { label: 'III', value: 'III' },
+            { label: 'IV', value: 'IV' },
+            { label: 'V', value: 'V' },
+          ],
+        },
+        email: {
+          className: 'w-50',
+          type: 'email',
+          label: 'Email',
+        },
+        password: {
+          className: 'w-50',
+          type: 'password',
+          label: 'Password',
+        },
       },
       buttons: [
         {
-          label: 'Log In',
+          label: 'Register',
           type: 'submit',
           classNames: 'btn btn-success btn-block text-white',
-          click: { type: 'LogIn' },
+          click: { type: 'Register' },
         },
       ],
     }
     this.registration = new Account()
   }
 
-  submit(event) {
-    event.payload.username = event.payload.email
-    event.payload.fullName = event.payload.firstName + ' ' + (event.payload.middleName || '') + ' ' + event.payload.lastName + ' ' + (event.payload.suffix || '')
-    return this.userApi.create(event.payload)
-      .subscribe(
-      (data) => {
-        this.ui.alerts.toastSuccess('Registration Success', `User <u><i>${event.payload.email}</u></i> has been registered successfully.`)
-        this.router.navigate(['auth', 'login'])
-      },
-      (err) => {
-        console.log(err)
-        this.ui.alerts.toastError('Registration Failure', err.statusCode === 401 ? 'Invalid Credentials' : err.message)
-      })
+  handleAction(event) {
+    switch (event.type) {
+      case 'Register': {
+        const fullName = new String(
+          `${event.payload.firstName} ${event.payload.middleName || ''} ${event
+            .payload.lastName} ${event.payload.suffix || ''}`
+        ).trim()
+        event.payload.fullName = fullName
+        event.payload.roles = []
+        return this.store.dispatch(new AuthActions.Register(event.payload))
+      }
+      default: {
+        return console.log('$event', event)
+      }
+    }
   }
-
 }
