a2545ccaf77da4b9c70ab2fe13ac4ebb94fc80d2
Brannon N. Darby II <bdarby86@gmail.com>
2017-09-08 22:30:41 -0400

    fix: test data

--- apps/api/server/boot/test-data.ts
+++ apps/api/server/boot/test-data.ts
@@ -16,25 +16,24 @@ class TestData {
 
     User.exists(adminId)
       .then((res: any) => {
-        console.log(res)
         if (res) {
-          return console.log('[test-data]', 'admin-user already exists')
+          return console.log('[test-data]', 'admin-user already exists: skipping creation')
         }
+        return Promise.all([
+          this.createData(User, [TestUsers]),
+          this.createData(Role, [TestRoles]),
+          this.createData(Control, [TestControls]),
+        ])
+          .then(() => Role.find({ where: { name: adminRole } }))
+          .then((role: any) => role[0].principals.create({ principalType: 'USER', principalId: adminId }))
+          .tap((res: any) => console.log('[test-data]', `${adminRole} role assigned to ${adminId}`))
       })
-      .then(() => Promise.all([
-        this.createData(User, [TestUsers]),
-        this.createData(Role, [TestRoles]),
-        this.createData(Control, [TestControls]),
-      ]))
-      .then(() => Role.find({ where: { name: adminRole } }))
-      .then((role: any) => role[0].principals.create({ principalType: 'USER', principalId: adminId }))
-      .tap((res: any) => console.log('[test-data]', `Roles ${adminRole} assigned to`))
   }
 
   createData(Model: any, data: any[]) {
     return Model.create(data)
       .tap((res: any) => console.log('[test-data]', `${res.length} items created for model`, Model.modelName))
-      .catch((err: any) => console.log('[test-data]', `Data could not be created for model`, Model.modelName, 'err:', err))
+      .catch((err: any) => console.log('[test-data]', `Data could not be created for model`, Model.modelName, ':', err))
   }
 }
 
